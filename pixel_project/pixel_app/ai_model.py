7c6c9ba171f661ebf0c928bf0f85f7e4cceae036
IsmiRosiaDwianti <ismirosia66@gmail.com>
2025-11-09 11:56:08 +0700

    Perbaikan versi pickle

--- pixel_project/pixel_app/ai_model.py
+++ pixel_project/pixel_app/ai_model.py
@@ -2,7 +2,7 @@ import joblib
 import os
 import numpy as np
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), 'processed_data', 'smart_lighting_model.pkl')
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'processed_data', 'smart_lighting_model_v4.pkl')
 model = joblib.load(MODEL_PATH)
 
 def predict_lighting_action(lux, motion, day, time_of_day, weather):
